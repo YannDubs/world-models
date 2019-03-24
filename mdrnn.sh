6122f0a4919ff6347f0d7c92e6ecba657652f0f1
Yann Dubois <yanndubois96@gmail.com>
2019-03-24 14:14:07 +0000

    CLEAN: remove scripts and logs.

--- mdrnn.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-log=logger-mdrnn.log
-
-echo "exp_dir" > $log
-python trainmdrnn.py --logdir exp_dir --epochs 20
-
-echo "factor_dir" > $log
-python trainmdrnn.py --logdir factor_dir --epochs 20
